f3de35b111dd9a102491fd9496d66f5b1679f061
Fer Fuentes <fernandocastillo.8@hotmail.com>
2025-05-17 09:06:45 -0600

    notificable interface

--- src/notifications/abstract/abstractNotification.ts
+++ src/notifications/abstract/abstractNotification.ts
@@ -1,8 +1,8 @@
-export interface Notifcable {
+export interface Notificable {
     sent(): Promise<string>;
 }
 
-export abstract class AbstractNotification implements Notifcable {
+export abstract class AbstractNotification implements Notificable {
     receiver: string;
     setReciever(receiver: string): void {
         this.receiver = receiver;
--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -1,30 +1,33 @@
 import { Controller } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
+import { EmailNotification } from './notifications/email-notification';
+import { SmsNotification } from './notifications/sms-notification';
+import { WhatsappNotification } from './notifications/whatsapp-notification';
+import { Body } from '@nestjs/common';
+import { Post } from '@nestjs/common';
+import { Notificable } from './abstract/abstractNotification'
 
 @Controller('notifications')
 export class NotificationsController {
-  constructor(private readonly notificationsService: NotificationsService) {}
+    constructor(private readonly notificationsService: NotificationsService) { }
 
     @Post()
     async create(@Body() createNotificationDto: CreateNotificationDto) {
-        let notificationType: Notifiable;
+        let notificationType: Notificable;
 
         switch (createNotificationDto.type) {
             case 'email':
-                notificationType = new EmailNotification();
+                notificationType = new EmailNotification(createNotificationDto.email);
                 break;
             case 'sms':
-                notificationType = new SMSNotification();
+                notificationType = new SmsNotification(createNotificationDto.phoneNumber, createNotificationDto.area);
                 break;
             case 'ws':
-                notificationType = new WhatsAppNotification();
+                notificationType = new WhatsappNotification(createNotificationDto.phoneNumber, createNotificationDto.area);
                 break;
             default:
                 throw new Error('Unknown notification type');
         }
-
-
-        notificationType.setReceiver(createNotificationDto.receiver);
 
         const result = await notificationType.sent();
         return result;
